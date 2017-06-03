846a2d180adf69fbad9ed7070d8254c095e64941
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2017-06-03 20:10:08 -0300

    Add functional tests

---
+++ tests/fake_code.py
@@ -0,0 +1,46 @@
+import os
+import abc
+
+
+def high_complexity (arg):
+    """High cyclomatic complexity."""
+    if arg == 0:
+        print(0)
+    elif arg == 1:
+        print(1)
+    elif arg == 2:
+        print(2)
+    elif arg == 3:
+        print(3)
+    elif arg == 4:
+        print(4)
+    elif arg == 5:
+        print(5)
+    elif arg == 6:
+        print(6)
+    elif arg == 7:
+        print(7)
+    elif arg == 8:
+        print(8)
+    elif arg == 9:
+        print(9)
+    elif arg == 10:
+        print(10)
+    elif arg == 11:
+        print(11)
+    elif arg == 12:
+        print(12)
+    elif arg == 13:
+        print(13)
+    elif arg == 14:
+        print(14)
+    elif arg == 15:
+        print(15)
+    elif arg == 16:
+        print(16)
+    elif arg == 17:
+        print(17)
+    elif arg == 18:
+        print(18)
+    elif arg == 19:
+        print(19)
---
+++ tests/test_functional.py
@@ -0,0 +1,84 @@
+"""Function tests for yala executable."""
+from concurrent.futures import ThreadPoolExecutor
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+
+from yala.main import Main
+
+
+class TestFunctional(TestCase):
+    """Functional test."""
+
+    @classmethod
+    @patch('yala.main.sys.exit')
+    @patch('yala.main.sys.stdout', new_callable=StringIO)
+    def setUpClass(cls, stdout_mock, exit_mock):
+        """Get yala's output to be used in tests.
+
+        As coverage outputs random results with --concurrency=multiprocessing,
+        we use Python threads instead.
+        """
+        # Ignore params of patch decorators:
+        # pylint: disable=arguments-differ
+        cls._exit = exit_mock
+        main = Main()
+        with patch('yala.main.Pool') as pool:
+            # Replace multiprocessing by Python threads
+            pool.return_value = ThreadPoolExecutor()
+            main.run(['tests/fake_code.py'])
+            output = stdout_mock.getvalue()
+        # Remove empty last line due to trailing '\n'
+        cls._output = output.split('\n')[:-1]
+
+    def _assert_results(self, lines, linter_name):
+        """Assert all lines are in the output."""
+        for line in lines:
+            self._assert_result(line, linter_name)
+
+    def _assert_result(self, line, linter_name):
+        full_line = f'tests/fake_code.py|{line} [{linter_name}]'
+        self.assertIn(full_line, self._output)
+
+    def test_exit_error(self):
+        """Should exit with error if there's linter output."""
+        arg = self._exit.call_args_list[0][0][0]
+        self.assertTrue(arg, 'Exit argument should not be empty')
+
+    def test_isort(self):
+        """Check isort ouput."""
+        expected = 'None:None|Imports are incorrectly sorted.'
+        self._assert_result(expected, 'isort')
+
+    def test_pycodestyle(self):
+        """Check pycodestyle output."""
+        expected = "5:20|E211 whitespace before '('"
+        self._assert_result(expected, 'pycodestyle')
+
+    def test_pydocstyle(self):
+        """Check pydocstyle ouput."""
+        expected = '1:None|D100: Missing docstring in public module'
+        self._assert_result(expected, 'pydocstyle')
+
+    def test_pyflakes(self):
+        """Check Pyflakes output."""
+        expected = (
+            "1:None|'os' imported but unused",
+            "2:None|'abc' imported but unused"
+        )
+        self._assert_results(expected, 'pyflakes')
+
+    def test_pylint(self):
+        """Check Pylint output."""
+        expected = (
+            '1:0|Missing module docstring (C0111, missing-docstring)',
+            '1:0|Unused import os (W0611, unused-import)',
+            '2:0|Unused import abc (W0611, unused-import)',
+            '5:0|Too many branches (20/12) (R0912, too-many-branches)'
+        )
+        self._assert_results(expected, 'pylint')
+
+    def test_rest_radon_cc(self):
+        """Check radon cc ouput."""
+        expected = '5:0|high_complexity - D'
+        self._assert_result(expected, 'radon cc')
