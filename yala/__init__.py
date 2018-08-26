832d6c9840d70fbd1dd1c9de80b6a2105b2f5556
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2018-08-25 21:16:14 -0300

    Release 1.7.0

--- yala/__init__.py
+++ yala/__init__.py
@@ -2,5 +2,7 @@
 from pathlib import Path
 import logging.config
 
+__version__ = '1.7.0'
+
 CONFIG_PATH = Path(__file__).parent / 'logging.ini'
 logging.config.fileConfig(str(CONFIG_PATH), disable_existing_loggers=False)
